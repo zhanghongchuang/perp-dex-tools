db2306c1c1300d3e317a3d958372b07121266c49
yourQuantGuy <yourquantguy.crypto@gmail.com>
2025-09-29 16:43:11 +0000

    fix order status update

--- trading_bot.py
+++ trading_bot.py
@@ -275,6 +275,11 @@ class TradingBot:
             ):
                 self.logger.log(f"[OPEN] [{order_id}] Waiting for order to be filled", "INFO")
                 await asyncio.sleep(5)
+                if self.config.exchange == "lighter":
+                    current_order_status = self.exchange_client.current_order.status
+                else:
+                    order_info = await self.exchange_client.get_order_info(order_id)
+                    current_order_status = order_info.status
                 new_order_price = await self.exchange_client.get_order_price(self.config.direction)
 
             self.order_canceled_event.clear()
